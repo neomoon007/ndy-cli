0612945bd37f66944ec62e9d85593f616d02ba59
Marcos Costa <costamarcos404@gmail.com>
2023-02-17 16:21:26 -0300

    Preparing the task.py file

--- src/commands/task.py
+++ src/commands/task.py
@@ -1,35 +1,29 @@
 # Tasks module, add, complete and remove tasks from the 'tasks' database
 from .. import database as db
 
-def add(name, date_input=0):
+def add(name, date_input=0): # Reacts to the `add` command
     # Insert a task with the name given into the DB
-    # db.cursor.execute("INSERT INTO tasks (name, done) VALUES (?, ?)", (name, False))
+    db.cursor.execute("INSERT INTO tasks (name, done) VALUES (?, ?)", (name, False))
     
     # Save the changes
-    # db.conn.commit()
+    db.conn.commit()
 
     # Close the DB connection
-    # db.cursor.close()
-    # db.conn.close()
-
-    # Debug
-    print("add()")
-    input("wait... ")
+    db.cursor.close()
+    db.conn.close()
 
 
-def edit(name):
-    print("edit()")
-    input("wait... ")
+def edit(name): # Reacts to the `edit` command
+    # WIP
 
 
-def complete(name):
-    print("complete()")
-    input("wait... ")
+def complete(name): # Reacts to the `comp` command
+    # WIP
 
 
-def delete(name):
-    print("delete()")
-    input("wait... ")
+def delete(name): # Reacts to the `del` command
+    # WIP
 
 
-
+def query(name): # Reacts to the `list` command
+    # WIP
