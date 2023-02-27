e701f1acd5530898e205346392a66dc8f8754ca3
Marcos Costa <costamarcos404@gmail.com>
2023-02-27 15:44:43 -0300

    Formatting src/database.py

--- src/database.py
+++ src/database.py
@@ -1,8 +1,8 @@
 # Database connection
 import sqlite3
 
-conn = sqlite3.connect("data/database.db") # Connects with the database
-cursor = conn.cursor() # Creates the cursor object to execute commands
+conn = sqlite3.connect("data/database.db")  # Connects with the database
+cursor = conn.cursor()  # Creates the cursor object to execute commands
 
 cursor.execute("""CREATE TABLE IF NOT EXISTS tasks
                   (id INTEGER PRIMARY KEY, name TEXT, done BOOLEAN)""")
